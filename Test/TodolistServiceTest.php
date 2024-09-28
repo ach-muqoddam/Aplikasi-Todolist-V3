4b9c9e835729bcb5c9ae2ba817d1bf53b092c0e4
adam <ach.adam04@gmail.com>
2024-09-29 00:40:44 +0700

    Testing service menambah todolist

--- Test/TodolistServiceTest.php
+++ Test/TodolistServiceTest.php
@@ -1,5 +1,6 @@
 <?php 
 
+require_once __DIR__ . "/../Config/Database.php";
 require_once __DIR__ . "/../Entity/Todolist.php";
 require_once __DIR__ . "/../Repository/TodolistRepository.php";
 require_once __DIR__ . "/../Service/TodolistService.php";
@@ -8,44 +9,46 @@ use Entity\Todolist;
 use Repository\TodolistRepositoryImpl;
 use Service\TodolistServiceImpl;
 
-function testShowTodolist(): void
-{
-    $todolistRepository = new TodolistRepositoryImpl();
-    $todolistRepository->todolist[1] = new Todolist("Belajar PHP Dasar");
-    $todolistRepository->todolist[2] = new Todolist("Belajar PHP OOP");
-    $todolistRepository->todolist[3] = new Todolist("Belajar PHP Database");
-    $todolistService = new TodolistServiceImpl($todolistRepository);
+//function testShowTodolist(): void
+//{
+    //$todolistRepository = new TodolistRepositoryImpl();
+    //$todolistRepository->todolist[1] = new Todolist("Belajar PHP Dasar");
+    //$todolistRepository->todolist[2] = new Todolist("Belajar PHP OOP");
+    //$todolistRepository->todolist[3] = new Todolist("Belajar PHP Database");
+    //$todolistService = new TodolistServiceImpl($todolistRepository);
 
-    $todolistService->showTodolist();
+    //$todolistService->showTodolist();
 
-}
+//}
 
 function testAddTodolist(): void 
 {
-    $todolistRepository = new TodolistRepositoryImpl();
+    // membuat parameter koneksi
+    $connection = \Config\Database::getConnection();
+    $todolistRepository = new TodolistRepositoryImpl($connection);
 
     $todolistService = new TodolistServiceImpl($todolistRepository);
     $todolistService->addTodolist("Belajar PHP Dasar");
     $todolistService->addTodolist("Belajar PHP OOP");
     $todolistService->addTodolist("Belajar PHP Database");
 
-    $todolistService->showTodolist();
+   // $todolistService->showTodolist();
 }
 
-function testRemoveTodolist(): void 
-{
-    $todolistRepository = new TodolistRepositoryImpl();
+// function testRemoveTodolist(): void 
+// {
+//     $todolistRepository = new TodolistRepositoryImpl();
 
-    $todolistService = new TodolistServiceImpl($todolistRepository);
-    $todolistService->addTodolist("Belajar PHP Dasar");
-    $todolistService->addTodolist("Belajar PHP OOP");
-    $todolistService->addTodolist("Belajar PHP Database");
+//     $todolistService = new TodolistServiceImpl($todolistRepository);
+//     $todolistService->addTodolist("Belajar PHP Dasar");
+//     $todolistService->addTodolist("Belajar PHP OOP");
+//     $todolistService->addTodolist("Belajar PHP Database");
 
-    $todolistService->showTodolist();
-    $todolistService->removeTodolist(1);
-    $todolistService->showTodolist();
-}
+//     $todolistService->showTodolist();
+//     $todolistService->removeTodolist(1);
+//     $todolistService->showTodolist();
+// }
 
-testRemoveTodolist();
+testAddTodolist();
 
 ?>
