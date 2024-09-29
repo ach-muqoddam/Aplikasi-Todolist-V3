8d987ea8b488c5b9db107206571434dbff9d9b0e
adam <ach.adam04@gmail.com>
2024-09-29 09:58:15 +0700

    Testing keseluruhan Aplikasi

--- App.php
+++ App.php
@@ -1,18 +1,23 @@
 <?php 
 
+require_once __DIR__ . "/Config/Database.php";
 require_once __DIR__ . "/Entity/Todolist.php";
 require_once __DIR__ . "/Helper/InputHelper.php";
 require_once __DIR__ . "/Repository/TodolistRepository.php";
 require_once __DIR__ . "/Service/TodolistService.php";
 require_once __DIR__ . "/View/TodolistView.php";
 
+use PSpell\Config;
 use Repository\TodolistRepositoryImpl;
 use Service\TodolistServiceImpl;
 use View\TodolistView;
 
 echo "APLIKASI TODOLIST v2" . PHP_EOL;
 
-$todolistRepository = new TodolistRepositoryImpl();
+// membuat parameter koneksi
+$connection = \Config\Database::getConnection();
+
+$todolistRepository = new TodolistRepositoryImpl($connection);
 $todolistService = new TodolistServiceImpl($todolistRepository);
 $todolistView = new TodolistView($todolistService);
 
